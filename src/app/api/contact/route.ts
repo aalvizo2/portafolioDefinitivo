8c8ce9e4c8296b128ed54f299d0acdb7f63cf398
Alan Alvizo <aalvizo24@hotmail.com>
2025-09-28 11:39:35 -0600

    any quitados de contact

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,56 +1,52 @@
 import nodemailer from 'nodemailer'
 
+type ContactRequest = {
+  name: string
+  email: string
+  message: string
+}
 
-export async function POST(req: { json: () => PromiseLike<{ name: any; email: any; message: any }> | { name: any; email: any; message: any } }){
-    try{
-       const {
-        name,
-        email,
-        message
-       }= await req.json()
+export async function POST(req: Request) {
+  try {
+    const { name, email, message }: ContactRequest = await req.json()
 
-       //Configuracion del transporter
-       const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
+    // Configuración del transporter
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    })
 
-       })
+    // Contenido del correo
+    await transporter.sendMail({
+      from: `"Portfolio Contact" <${email}>`,
+      to: process.env.EMAIL_TO,
+      subject: `New Message from ${name}`,
+      text: message,
+      html: `
+        <h3>New Message from your portfolio</h3>
+        <p><b>Name: </b> ${name}</p>
+        <p><b>Message: </b> ${message}</p>
+      `,
+    })
 
-       //Contenido del Correo
-       await transporter.sendMail({
-        from: `"Portfolio Contact" <${email}>`,
-        to: process.env.EMAIL_TO,
-        subject: `New Message from ${name}`,
-        text: message,
-        html: `
-           <h3>New Message from your portfolio</h3>
-           <p><b>Name: </b> ${name}</p>
-           <p><b>Message: </b> ${message}</p>
-        `
-       })
-
-       return new Response(
-         JSON.stringify({
-            success: true, 
-            message: 'Email sent successfully!'
-         }),
-         {
-            status: 200
-         }
-       )
-    }catch(error){
-       console.error(error)
-       return new Response(
-        JSON.stringify({
-            success: false,
-            message: 'Error sending email.'
-        }),
-        {
-            status: 500
-        }
-       )
-    }
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: 'Email sent successfully!'
+      }),
+      { status: 200 }
+    )
+  } catch (error) {
+    console.error(error)
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: 'Error sending email.'
+      }),
+      { status: 500 }
+    )
+  }
 }
